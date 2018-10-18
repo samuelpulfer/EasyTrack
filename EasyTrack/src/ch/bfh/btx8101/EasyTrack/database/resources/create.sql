e5a086a9262631684b34b31402031f2b7601eb73
Samuel <Samuel@NB544957>
2018-10-19 00:41:42 +0200

    create.sql updated

--- EasyTrack/src/ch/bfh/btx8101/EasyTrack/database/resources/create.sql
+++ EasyTrack/src/ch/bfh/btx8101/EasyTrack/database/resources/create.sql
@@ -10,3 +10,23 @@ CREATE TABLE `destinations` (
   `modified` DATETIME NULL,
   `deleted` DATETIME NULL
 );
+CREATE TABLE `order` (
+  `id` INT AUTO_INCREMENT PRIMARY KEY,
+  `fid` VARCHAR(20) NOT NULL,
+  `destination_start` INTEGER NOT NULL,
+  `destination_end` INTEGER NOT NULL,
+  `ts_start` DATETIME NULL,
+  `ts_end` DATETIME NULL,
+  `mode` VARCHAR(20) NOT NULL,
+  `isolation` BOOL,
+  `emergency` BOOL,
+  `message` VARCHAR(500),
+  `accepted` DATETIME NULL,
+  `pickedup` DATETIME NULL,
+  `delivered` DATETIME NULL,
+  `carrier` VARCHAR(30) NULL,
+  `creator` VARCHAR(30) NULL,
+  `created` DATETIME NOT NULL,
+  `modified` DATETIME NULL,
+  `deleted` DATETIME NULL
+);
